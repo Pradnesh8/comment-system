f06d406773c756a1a34e675bb8d71dab9453ee67
Pradnesh Khedekar <pradneshkhedekar08@gmail.com>
2024-09-13 01:35:45 +0530

    comment CRUD firebase methods

--- src/utils/firebaseDb.utils.js
+++ src/utils/firebaseDb.utils.js
@@ -1,6 +1,6 @@
 
-import { collection, addDoc } from "firebase/firestore";
-import { db } from './utils/firebase.utils'
+import { collection, addDoc, getDocs, updateDoc, deleteDoc, doc } from "firebase/firestore";
+import { db } from './firebase.utils'
 
 export const addData = async () => {
     try {
@@ -33,3 +33,45 @@ export const deleteData = async (id) => {
     await deleteDoc(doc(db, "users", id));
 };
 
+// CRUD for comments collection
+
+export const addComment = async (data) => {
+    try {
+        const docRef = await addDoc(collection(db, "comments"), {
+            name: data.name,
+            email: data.email,
+            content: data.content,
+            reactions: [],
+            replyFlag: data.replyFlag,
+            userPicture: data.photoURL
+        });
+        console.log("Document written with ID: ", docRef.id);
+        return docRef;
+    } catch (e) {
+        console.error("Error adding document: ", e);
+        return ""
+    }
+};
+
+export const getComments = async () => {
+    const querySnapshot = await getDocs(collection(db, "comments"));
+    const comments = []
+    console.log(querySnapshot)
+    querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${doc.data()}`);
+        comments.push(doc.data());
+    });
+    return comments;
+};
+
+export const updateComment = async (id, data) => {
+    const userRef = doc(db, "comments", id);
+    await updateDoc(userRef, {
+        reactions: data.reactions
+    });
+};
+
+
+export const deleteComment = async (id) => {
+    await deleteDoc(doc(db, "comments", id));
+};
