20030fddbca5aea6f9c2c4aaf82f988665197c01
Pradnesh Khedekar <pradneshkhedekar08@gmail.com>
2024-09-15 17:33:07 +0530

    added html-react-parser to parse html content of saved comments using quill

--- src/components/CommentPost.jsx
+++ src/components/CommentPost.jsx
@@ -1,14 +1,24 @@
 import React, { useContext } from 'react'
 import Profile from './Profile'
 import userContext from "../utils/userContext";
-import { displayName } from 'react-quill';
+// import { displayName } from 'react-quill';
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+// import { ReactMarkdown } from 'react-markdown/with-html'
+import Markdown from 'react-markdown'
+import parse from 'html-react-parser';
+// const ReactMarkdown = require("react-markdown/with-html"); //for displaying html
 const CommentPost = ({ post }) => {
     const { content, email, name, reactions, replyFlag, userPicture } = post;
     const { user, setUser } = useContext(userContext);
     return (
         <div className='comment-post'>
             <Profile user={{ email, displayName: name, photoURL: userPicture }} />
-            <div className='comment-content'>{content}
+            <div className='comment-content'>
+                {/* <div className="ql-editor" style={{ padding: 0 }}>
+                    <Markdown skipHtml={true}>{content}</Markdown>
+                </div> */}
+                {parse(content)}
                 <span className='show-more-btn'><br />show more</span>
             </div>
             {/* Footer - emoji icon | reply | time since post */}
